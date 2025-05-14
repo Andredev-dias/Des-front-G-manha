5629a7e50f6f518ce4c34ae3a5ac2ab6ae99de28
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2025-05-14 11:30:41 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,29 @@ import style from './App.module.css'
 function App() {
   return (
     <>
-      <h1 className={style.title}>Olá mundo</h1>
+      <nav className={style.menu}>
+        <p><a href="#s1">Apresentação</a></p>
+        <p><a href="#s2">Tecnologias</a></p>
+        <p><a href="#s3">Projetos</a></p>
+        <p><a href="#s4">Contato</a></p>
+      </nav>
+      <main>
+        <section className={style.s1} id='s1'>
+          <h2>Apresentação</h2>
+        </section>
+        <section className={style.s2} id='s2'>
+          <h2>Tecnologias</h2>
+        </section>
+        <section className={style.s3} id='s3'>
+          <h2>Projetos</h2>
+        </section>
+        <section className={style.s4} id='s4'>
+          <h2>Contato</h2>
+        </section>
+      </main>
+      <footer>
+        <h3>redes sociais</h3>
+      </footer>
     </>
   )
 }
